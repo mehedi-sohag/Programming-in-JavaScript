63739d7a0e82c2ed7de5706467f5d2eca5b5af76
mehedi-sohag <mehedi.sohag@yahoo.com>
2021-11-13 04:07:36 -0800

    array destructing

---
+++ Lecture-04/script.js
@@ -0,0 +1,20 @@
+const arr = [2, 3, 4, 5, 6];
+const user = {
+  name: 'mehedi',
+  age: 23,
+  friends: ['alice', 'bob', 'roton', 'hasan', 'era'],
+  check: function (one, two) {
+    return [this.friends[one], this.friends[two]];
+  },
+};
+
+console.log(arr);
+console.log(arr[0]);
+//destructuring an array form existing array
+
+const [a, , b, , c] = arr;
+console.log(a, b, c);
+
+const [friend1, friend2] = user.check(2, 3);
+console.log(friend1, friend2);
+console.log(user.check(1, 2));
